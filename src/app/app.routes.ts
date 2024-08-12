74e6c977bfcdb4b84f13357bb7e730e4c952cdc4
Abhinav Nath Gupta <abhi.aec89@gmail.com>
2024-08-12 21:30:01 +0530

    Added More Components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CreateTodoComponent } from '../create-todo/create-todo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: AppComponent },
+  { path: 'add', component: CreateTodoComponent },
+  { path: 'edit', component: CreateTodoComponent },
+];
--- src/todos/todos.component.ts
+++ src/todos/todos.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-
+interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
 @Component({
   selector: 'app-todos',
   standalone: true,
@@ -8,5 +12,18 @@ import { Component } from '@angular/core';
   styleUrl: './todos.component.sass'
 })
 export class TodosComponent {
+todos: Todo[] = [];
+  newTodoText = '';
 
+  addTodo() {
+    // Add new todo logic
+  }
+
+  removeTodo(id: number) {
+    // Remove todo logic
+  }
+
+  toggleTodo(id: number) {
+    // Toggle todo completion logic
+  }
 }
