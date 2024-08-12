914784c563c713b9106794a5238b93230f9f2b15
Abhinav <tech01@publiqverse.io>
2024-08-12 21:50:12 +0530

    :sparkles: (Format on Save)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,10 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { CreateTodoComponent } from '../create-todo/create-todo.component';
 import { UpdateTodoComponent } from '../update-todo/update-todo.component';
 import { TodosComponent } from '../todos/todos.component';
 
 export const routes: Routes = [
-    { path: 'todo', component: TodosComponent },
+  { path: 'todo', component: TodosComponent },
   { path: 'add', component: CreateTodoComponent },
   { path: 'update', component: UpdateTodoComponent },
 ];
