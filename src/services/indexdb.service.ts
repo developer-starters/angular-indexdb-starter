f25c4e2e6699c3c8d40cb53d1e296da78002f19f
Abhinav Nath Gupta <abhi.aec89@gmail.com>
2024-08-17 15:14:57 +0530

    List Todo and Delete Todo Completed

--- src/services/indexdb.service.ts
+++ src/services/indexdb.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { db } from './app-db';
+import { Todo } from '../interfaces/todo';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +16,25 @@ export class IndexdbService {
 
   async getTodos() {
     return db.todo.toArray()
+  }
+
+  async getTodoById(id: number) {
+    return db.todo.where({
+      id: id
+    });
+  }
+
+  async deleteTodo(id: number) {
+    return db.todo.delete(id);
+  }
+
+  async updateTodo(id: number, text?: string, completed?: boolean) {
+    const updatedTodo: Todo = {
+      id: id,
+      text: text ?? '',
+      completed: completed ?? false
+    };
+    return db.todo.put(updatedTodo, id);
   }
 
 }
--- src/todos/todos.component.ts
+++ src/todos/todos.component.ts
@@ -5,32 +5,42 @@ import { FormsModule } from '@angular/forms';
 import { v4 as uuid } from 'uuid';
 import { IndexdbService } from '../services/indexdb.service';
 import { Todo } from '../interfaces/todo';
+import { liveQuery } from 'dexie';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [FontAwesomeModule, FormsModule],
+  imports: [FontAwesomeModule, FormsModule, CommonModule],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.sass'
 })
 export class TodosComponent {
   newTodoText = '';
   faCoffee = faCoffee;
+  todos$;
 
   constructor(public db: IndexdbService) {
     // Initialize todos from local storage or API
     this.db.getTodos().then((todos) => {
       console.log(todos);
     });
+    this.todos$ = liveQuery(() => this.db.getTodos());
+  }
+
+  identifyList(index: number, list: Todo) {
+    return `${list.id}${list.text}`;
   }
 
   addTodo() {
     // Add new todo logic
     console.log(`This is the new Todo Task Added ${this.newTodoText}`);
     this.db.addTodo(this.newTodoText);
+    this.newTodoText = '';
   }
 
-  removeTodo(id: string) {
+  removeTodo(id: number) {
     // Remove todo logic
+    this.db.deleteTodo(id);
   }
 
   toggleTodo(id: string) {
