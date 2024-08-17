6c90d68010e0cce8e9b5b4be7ba5b340cd787943
Abhinav Nath Gupta <abhi.aec89@gmail.com>
2024-08-17 13:06:20 +0530

    Saving Todo is working

--- src/todos/todos.component.ts
+++ src/todos/todos.component.ts
@@ -1,32 +1,48 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { FormsModule } from '@angular/forms';
+import { v4 as uuid } from 'uuid';
+import { IndexdbService } from '../services/indexdb.service';
+
 interface Todo {
-  id: number;
+  id: string;
   text: string;
   completed: boolean;
 }
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, FormsModule],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.sass'
 })
 export class TodosComponent {
-todos: Todo[] = [];
+  todos: Todo[] = [];
   newTodoText = '';
   faCoffee = faCoffee;
 
-  addTodo() {
-    // Add new todo logic
+  constructor(public db: IndexdbService) {
+    // Initialize todos from local storage or API
   }
 
-  removeTodo(id: number) {
+  addTodo() {
+    // Add new todo logic
+    console.log(`This is the new Todo Task Added ${this.newTodoText}`);
+    const newTodo: Todo = {
+      id: uuid(),
+      text: this.newTodoText,
+      completed: false
+    };
+    this.todos.push(newTodo);
+    this.db.addTodo(JSON.stringify(newTodo));
+  }
+
+  removeTodo(id: string) {
     // Remove todo logic
   }
 
-  toggleTodo(id: number) {
+  toggleTodo(id: string) {
     // Toggle todo completion logic
   }
 }
