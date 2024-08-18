7b6efbf6ba1dbdc09166756a23c672db273a5369
Abhinav <tech01@publiqverse.io>
2024-08-18 18:00:50 +0530

    Removed Unused Files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,6 @@
 import { Routes } from '@angular/router';
-import { CreateTodoComponent } from '../create-todo/create-todo.component';
-import { UpdateTodoComponent } from '../update-todo/update-todo.component';
 import { TodosComponent } from '../todos/todos.component';
 
 export const routes: Routes = [
   { path: '', component: TodosComponent },
-  { path: 'todo', component: TodosComponent },
-  { path: 'add', component: CreateTodoComponent },
-  { path: 'update', component: UpdateTodoComponent },
 ];
